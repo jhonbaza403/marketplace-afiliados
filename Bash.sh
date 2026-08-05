bfa01324c1f6c53f58124202674e316b5004da65
Jhon baza <bazwjhon@gmail.com>
2026-08-05 08:39:45 -0400

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -2,3 +2,6 @@ bun install
 git add .
 git commit -m "Corregir rutas de importacion y tsconfig"
 git push origin main
+git add package.json
+git commit -m "Corregir sintaxis en package.json"
+git push origin main
